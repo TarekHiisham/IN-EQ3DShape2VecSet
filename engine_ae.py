8f2b3e1836f289f9705a27cbcb964881ebd466d1
TarekHiisham <tarekhesham2017@gmail.com>
2026-09-23 14:35:10 +0200

    first version of Invariant

---
+++ engine_ae.py
@@ -0,0 +1,206 @@
+# --------------------------------------------------------
+# References:
+# MAE: https://github.com/facebookresearch/mae
+# DeiT: https://github.com/facebookresearch/deit
+# BEiT: https://github.com/microsoft/unilm/tree/master/beit
+# --------------------------------------------------------
+
+import math
+import sys
+from typing import Iterable
+
+import torch
+import torch.nn.functional as F
+import e3nn.o3 as o3
+
+import util.misc as misc
+import util.lr_sched as lr_sched
+
+
+def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module, criterion_inv: torch.nn.Module,
+                    data_loader: Iterable, optimizer: torch.optim.Optimizer,
+                    device: torch.device, epoch: int, loss_scaler, max_norm: float = 0,
+                    log_writer=None, args=None):
+    model.train(True)
+    metric_logger = misc.MetricLogger(delimiter="  ")
+    metric_logger.add_meter('lr', misc.SmoothedValue(window_size=1, fmt='{value:.6f}'))
+    header = 'Epoch: [{}]'.format(epoch)
+    print_freq = 20
+
+    accum_iter = args.accum_iter
+
+    optimizer.zero_grad()
+
+    kl_weight = 1e-3
+
+    if log_writer is not None:
+        print('log_dir: {}'.format(log_writer.log_dir))
+
+    for data_iter_step, (points, labels, surface) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
+
+        # we use a per iteration (instead of per epoch) lr scheduler
+        if data_iter_step % accum_iter == 0:
+            lr_sched.adjust_learning_rate(optimizer, data_iter_step / len(data_loader) + epoch, args)
+
+        points = points.to(device, non_blocking=True)
+        labels = labels.to(device, non_blocking=True)
+        surface = surface.to(device, non_blocking=True)
+
+        R = o3.rand_matrix(args.batch_size, dtype=points.dtype)
+        R = R.to(device)
+
+        points_rot = torch.einsum('bij, bnj -> bni', R, points)
+        surface_rot = torch.einsum('bij, bnj -> bni', R, surface)
+        
+        with torch.cuda.amp.autocast(enabled=False):
+            outputs = model(surface, points)
+            outputs_rot = model(surface_rot, points_rot)
+            
+            if 'kl' in outputs:
+                loss_kl = outputs['kl']
+                loss_kl = torch.sum(loss_kl) / loss_kl.shape[0]
+            else:
+                loss_kl = None
+
+            outputs = outputs['logits']
+            outputs_rot = outputs_rot['logits']
+
+            loss_inv = criterion_inv(outputs, outputs_rot)
+            loss_vol = criterion(outputs[:, :1024], labels[:, :1024])
+            loss_near = criterion(outputs[:, 1024:], labels[:, 1024:])
+
+            
+            if loss_kl is not None:
+                loss = loss_vol + 0.1 * loss_near + kl_weight * loss_kl + loss_inv
+            else:
+                loss = loss_vol + 0.1 * loss_near + 2 * loss_inv
+
+        loss_value = loss.item()
+
+        threshold = 0
+
+        pred = torch.zeros_like(outputs[:, :1024])
+        pred[outputs[:, :1024]>=threshold] = 1
+
+        accuracy = (pred==labels[:, :1024]).float().sum(dim=1) / labels[:, :1024].shape[1]
+        accuracy = accuracy.mean()
+        intersection = (pred * labels[:, :1024]).sum(dim=1)
+        union = (pred + labels[:, :1024]).gt(0).sum(dim=1) + 1e-5
+        iou = intersection * 1.0 / union
+        iou = iou.mean()
+
+        if not math.isfinite(loss_value):
+            print("Loss is {}, stopping training".format(loss_value))
+            sys.exit(1)
+
+        loss /= accum_iter
+        loss_scaler(loss, optimizer, clip_grad=max_norm,
+                    parameters=model.parameters(), create_graph=False,
+                    update_grad=(data_iter_step + 1) % accum_iter == 0)
+        if (data_iter_step + 1) % accum_iter == 0:
+            optimizer.zero_grad()
+
+        torch.cuda.synchronize()
+
+        metric_logger.update(loss=loss_value)
+
+        metric_logger.update(loss_vol=loss_vol.item())
+        metric_logger.update(loss_near=loss_near.item())
+        metric_logger.update(loss_inv=loss_inv.item())
+
+        if loss_kl is not None:
+            metric_logger.update(loss_kl=loss_kl.item())
+
+        metric_logger.update(iou=iou.item())
+
+        min_lr = 10.
+        max_lr = 0.
+        for group in optimizer.param_groups:
+            min_lr = min(min_lr, group["lr"])
+            max_lr = max(max_lr, group["lr"])
+
+        metric_logger.update(lr=max_lr)
+
+        loss_value_reduce = misc.all_reduce_mean(loss_value)
+        if log_writer is not None and (data_iter_step + 1) % accum_iter == 0:
+            """ We use epoch_1000x as the x-axis in tensorboard.
+            This calibrates different curves when batch size changes.
+            """
+            epoch_1000x = int((data_iter_step / len(data_loader) + epoch) * 1000)
+            log_writer.add_scalar('loss', loss_value_reduce, epoch_1000x)
+            log_writer.add_scalar('lr', max_lr, epoch_1000x)
+
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print("Averaged stats:", metric_logger)
+    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
+
+
+@torch.no_grad()
+def evaluate(data_loader, model, device):
+    criterion = torch.nn.BCEWithLogitsLoss()
+    criterion_inv = torch.nn.MSELoss()
+
+    metric_logger = misc.MetricLogger(delimiter="  ")
+    header = 'Test:'
+
+    # switch to evaluation mode
+    model.eval()
+
+    for points, labels, surface, _ in metric_logger.log_every(data_loader, 50, header):
+
+        points = points.to(device, non_blocking=True)
+        labels = labels.to(device, non_blocking=True)
+        surface = surface.to(device, non_blocking=True)
+
+        R = o3.rand_matrix(points.shape[0], dtype=points.dtype)
+        R = R.to(device)
+
+        points_rot = torch.einsum('bij, bnj -> bni', R, points)
+        surface_rot = torch.einsum('bij, bnj -> bni', R, surface)
+
+        # compute output
+        with torch.cuda.amp.autocast(enabled=False):
+
+            outputs = model(surface, points)
+            outputs_rot = model(surface_rot, points_rot)
+
+            if 'kl' in outputs:
+                loss_kl = outputs['kl']
+                loss_kl = torch.sum(loss_kl) / loss_kl.shape[0]
+            else:
+                loss_kl = None
+
+            outputs = outputs['logits']
+            outputs_rot = outputs_rot['logits']
+
+            loss = criterion(outputs, labels)
+            loss_inv = criterion_inv(outputs, outputs_rot)
+
+        threshold = 0
+
+        pred = torch.zeros_like(outputs)
+        pred[outputs>=threshold] = 1
+
+        accuracy = (pred==labels).float().sum(dim=1) / labels.shape[1]
+        accuracy = accuracy.mean()
+        intersection = (pred * labels).sum(dim=1)
+        union = (pred + labels).gt(0).sum(dim=1)
+        iou = intersection * 1.0 / union + 1e-5
+        iou = iou.mean()
+
+        batch_size = points.shape[0]
+        metric_logger.update(loss=loss.item())
+        metric_logger.update(loss_inv=loss_inv.item())
+
+        metric_logger.meters['iou'].update(iou.item(), n=batch_size)
+
+        if loss_kl is not None:
+            metric_logger.update(loss_kl=loss_kl.item())
+
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print('* iou {iou.global_avg:.3f} loss {losses.global_avg:.3f} loss_inv {losses_inv.global_avg:.7f}'
+          .format(iou=metric_logger.iou, losses=metric_logger.loss, losses_inv=metric_logger.loss_inv))
+
+    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
---
+++ util/shapenet.py
@@ -0,0 +1,123 @@
+
+import os
+import glob
+import torch
+from torch.utils import data
+import numpy as np
+
+
+class ShapeNet(data.Dataset):
+
+    def __init__(
+        self,
+        mesh_folder,
+        point_folder,
+        split='train',
+        category_id='03797390',
+        num_samples=512,
+        pc_size=1024,
+        return_surface=True,
+        surface_sampling=True,
+        sampling=True,
+        transform=None,
+        train_ratio=0.8,
+        val_ratio=0.1,
+        seed=42,
+        replica=1
+    ):
+
+        self.split = split
+        self.num_samples = num_samples
+        self.pc_size = pc_size
+        self.return_surface = return_surface
+        self.surface_sampling = surface_sampling
+        self.sampling = sampling
+        self.transform = transform
+        self.replica=replica
+
+        self.mesh_folder = os.path.join(mesh_folder, category_id, '4_pointcloud')
+        self.point_folder = os.path.join(point_folder, 'ShapeNetV2_point', category_id)
+        
+        self.models = glob.glob(os.path.join(self.point_folder, '*.npz'))
+        rng = np.random.default_rng(seed)
+        rng.shuffle(self.models)
+
+        n_total = len(self.models)
+        n_train = int(train_ratio * n_total)
+        n_val = int(val_ratio * n_total)
+        
+        if split == 'train':
+            self.models = self.models[:n_train]
+
+        elif split == 'val':
+            self.models = self.models[n_train: n_train + n_val]
+
+        elif split == 'test':
+            self.models = self.models[n_train + n_val:]
+
+        else:
+            raise ValueError(f"Unknown split: {split}.")
+
+    def __len__(self):
+        if self.split != 'train':
+            return len(self.models)
+        else:
+            return len(self.models) * self.replica
+
+    def __getitem__(self, idx):
+        idx = idx % len(self.models)
+
+        model_path = self.models[idx]
+        try:
+            with np.load(model_path) as data:
+                vol_points = data['vol_points']
+                vol_label = data['vol_label']
+                near_points = data['near_points']
+                near_label = data['near_label']
+        except Exception as e:
+            print(e)
+            print(model_path)
+
+        with open(model_path.replace('.npz', '.npy'), 'rb') as f:
+            scale = np.load(f).item()
+
+        if self.return_surface:
+            model_idx = os.path.basename(model_path)
+            pc_path = os.path.join(self.mesh_folder, model_idx)
+            with np.load(pc_path) as data:
+                surface = data['points'].astype(np.float32)
+                surface = surface * scale
+            if self.surface_sampling:
+                ind = np.random.default_rng().choice(surface.shape[0], self.pc_size, replace=False)
+                surface = surface[ind]
+            surface = torch.from_numpy(surface)
+
+        if self.sampling:
+            ind = np.random.default_rng().choice(vol_points.shape[0], self.num_samples, replace=False)
+            vol_points = vol_points[ind]
+            vol_label = vol_label[ind]
+
+            ind = np.random.default_rng().choice(near_points.shape[0], self.num_samples, replace=False)
+            near_points = near_points[ind]
+            near_label = near_label[ind]
+
+        vol_points = torch.from_numpy(vol_points)
+        vol_label = torch.from_numpy(vol_label).float()
+
+        if self.split == 'train':
+            near_points = torch.from_numpy(near_points)
+            near_label = torch.from_numpy(near_label).float()
+
+            points = torch.cat([vol_points, near_points], dim=0)
+            labels = torch.cat([vol_label, near_label], dim=0)
+        else:
+            points = vol_points
+            labels = vol_label
+
+        if self.transform:
+            surface, points = self.transform(surface, points)
+
+        if self.return_surface:
+            return points, labels, surface
+        else:
+            return points, labels
