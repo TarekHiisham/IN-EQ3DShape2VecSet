c095917176e0de48e9decbca7b9ab0cba45e75a3
TarekHiisham <tarekhesham2017@gmail.com>
2026-09-23 23:07:03 +0200

    change output dir

--- util/misc.py
+++ util/misc.py
@@ -292,9 +292,8 @@ def save_model(args, epoch, model, model_without_ddp, optimizer, loss_scaler):
     output_dir = Path(args.output_dir)
     epoch_name = str(epoch)
     if loss_scaler is not None:
-        checkpoint_paths = [output_dir / ('checkpoint-%s.pth' % epoch_name)]
-        for checkpoint_path in checkpoint_paths:
-            to_save = {
+        checkpoint_path = output_dir / 'checkpoint.pth'
+        to_save = {
                 'model': model_without_ddp.state_dict(),
                 'optimizer': optimizer.state_dict(),
                 'epoch': epoch,
@@ -302,10 +301,10 @@ def save_model(args, epoch, model, model_without_ddp, optimizer, loss_scaler):
                 'args': args,
             }
 
-            save_on_master(to_save, checkpoint_path)
+        save_on_master(to_save, checkpoint_path)
     else:
         client_state = {'epoch': epoch}
-        model.save_checkpoint(save_dir=args.output_dir, tag="checkpoint-%s" % epoch_name, client_state=client_state)
+        model.save_checkpoint(save_dir=args.output_dir, tag="checkpoint", client_state=client_state)
 
 
 def load_model(args, model_without_ddp, optimizer, loss_scaler):
